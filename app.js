57cdea9f550365ae8051098b73f7b4c804ace136
Sutoni <t.sutoni@yahoo.com>
2018-11-17 16:36:27 +0700

    app js diload

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ app.set("view engine", "ejs");
 //setup public folder
 app.use(express.static(path.join(__dirname,"public")));
 
+//Setup index
+app.get("/",function(req, res){
+    res.send("ini adalah index yaa")
+});
+
+//Setup server
+var port = 3000;
+app.listen(port, function(){
+    console.log("Server running on port" + port);
+});
